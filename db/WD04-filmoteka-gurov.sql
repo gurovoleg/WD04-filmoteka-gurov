f15494f5637802ff696f0b887cd3d51ac79e1ab7
Oleg Gurov <oleggurov@CurovaOlga.local>
2018-09-17 00:17:45 +0300

    DB updated
    uploaded the latest data to the DB folder

--- db/WD04-filmoteka-gurov.sql
+++ db/WD04-filmoteka-gurov.sql
@@ -2,10 +2,10 @@
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Sep 15, 2018 at 09:53 PM
--- Server version: 5.7.23
--- PHP Version: 7.2.8
+-- Хост: localhost:8889
+-- Время создания: Сен 16 2018 г., 21:15
+-- Версия сервера: 5.7.23
+-- Версия PHP: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `WD04-filmoteka-gurov`
+-- База данных: `WD04-filmoteka-gurov`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `films`
+-- Структура таблицы `films`
 --
 
 CREATE TABLE `films` (
@@ -34,34 +34,34 @@ CREATE TABLE `films` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `films`
+-- Дамп данных таблицы `films`
 --
 
 INSERT INTO `films` (`id`, `name`, `genre`, `year`) VALUES
-(1, 'Дело храбрых', 'драма', 2017),
 (2, 'По соображениям совести', 'драма', 2016),
 (5, '50 первых поцелуев', 'комедия', 2004),
-(6, 'Интерстеллар', 'фантастика', 2014);
+(47, 'Интерстеллар', 'фантастика', 2015),
+(67, 'Дело храбрых', 'драма', 2017);
 
 --
--- Indexes for dumped tables
+-- Индексы сохранённых таблиц
 --
 
 --
--- Indexes for table `films`
+-- Индексы таблицы `films`
 --
 ALTER TABLE `films`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- AUTO_INCREMENT for table `films`
+-- AUTO_INCREMENT для таблицы `films`
 --
 ALTER TABLE `films`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
